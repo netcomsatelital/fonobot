ac60717eb90059b551d05ec4a6810dc4d786fc52
Claudio J. Peña <claudio@netcomsatelital.com.ar>
2019-01-19 16:57:08 -0300

    v0.0.16
    - Cambiados todos los colores y otras cuestiones estéticas.
    - Implementado SASS

--- js/audio.js
+++ js/audio.js
@@ -1,29 +1,23 @@
 $(document).ready(function() {
-
-  $('.audio-thumbnail audio').on('play', function () {
-    $(this).prev('a').find('.glyphicon').removeClass('glyphicon-play').addClass('glyphicon-stop');
-  });
-
-  var func_on_stop = function () {
-    $(this).prev('a').find('.glyphicon').removeClass('glyphicon-stop').addClass('glyphicon-play');
-  };
-  $('.audio-thumbnail audio').on('pause', func_on_stop);
-  $('.audio-thumbnail audio').on('ended', func_on_stop);
-
-
-  $('.audio-thumbnail audio').removeAttr('controls');
-
-  $('.audio-playback-control').click(function(e) {
-    e.preventDefault();
-    var audio_list = $('audio');
-    var audio_cur  = $(this).next('audio').get(0);
-    for (var x = 0; x < audio_list.length; x++) {
-      var audio = audio_list[x];
-      if (audio == audio_cur && audio.paused) {
-        audio.currentTime = 0;
-        audio.play();
-      } else if (!audio.paused)
-        audio.pause();
+  var audios = document.getElementsByTagName('audio');
+  for (var x = 0; x < audios.length; x++) {
+    var audio = audios[x];
+    if (audio.className == 'audio-control') {
+      audio.addEventListener('playing', function (audio_list) {
+        return function () {
+          for (var x = 0; x < audio_list.length; x++) {
+            var audio_item = audio_list[x];
+            if (audio_item != this) {
+              audio_item.pause();
+            }
+          }
+          //this.className = 'audio-control-playing';
+        };
+      }(audios), false);
+      audio.addEventListener('pause', function () {
+        //this.className = 'audio-control';
+        //this.currentTime = 0;
+      }, false);
     }
-  });
+  }
 });
